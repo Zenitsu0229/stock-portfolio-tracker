ce8a303846f999f81d3a882bf73747859e7baf92
Zenitsu0229 <kuyu0903@gmail.com>
2026-06-06 22:49:57 +0900

    chore: remove bin directory from tracking

--- bin/main/sql/insert_trade_named.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO trade_diary (
-    trade_date, symbol, side, entry_reason, rule_observance,
-    emotion, profit_loss, comment, created_at
-) VALUES (
-    :tradeDate, :symbol, :side, :entryReason, :ruleObservance,
-    :emotion, :profitLoss, :comment, :createdAt
-);
